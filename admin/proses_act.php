64bfd2edee321cbe45aa6d296b07d791232f6767
SANTISANTI1 <118633213+SANTISANTI1@users.noreply.github.com>
2023-04-10 20:35:06 +0700

    proses-act.php

--- admin/proses_act.php
+++ admin/proses_act.php
@@ -1,26 +1,23 @@
 <?php
 // mengaktifkan session pada php
 session_start();
-include 'koneksi.php';
+include "../koneksi.php";
 $limit = 10 * 1024 * 1024;
-$ekstensi =  array('png','jpg','jpeg','gif');
+// $ekstensi =  array('png','jpg','jpeg','gif');
 $jumlahFile = count($_FILES['foto']['name']);
+$ruangan = $_POST['nama_ruangan'];
  
 for($x=0; $x<$jumlahFile; $x++){
 	$namafile = $_FILES['foto']['name'][$x];
 	$tmp = $_FILES['foto']['tmp_name'][$x];
-	$tipe_file = pathinfo($namafile, PATHINFO_EXTENSION);
+	// $tipe_file = pathinfo($namafile, PATHINFO_EXTENSION);
 	$ukuran = $_FILES['foto']['size'][$x];	
 	if($ukuran > $limit){
 		header("location:tambahruangan.php?alert=gagal_ukuran");		
-	}else{
-		if(!in_array($tipe_file, $ekstensi)){
-			header("location:tambahruangan.php?alert=gagal_ektensi");			
 		}else{		
-			move_uploaded_file($tmp, 'file/'.date('d-m-Y').'-'.$namafile);
+			move_uploaded_file($tmp, '../file/'.date('d-m-Y').'-'.$namafile);
 			$x = date('d-m-Y').'-'.$namafile;
-			mysqli_query($koneksi,"INSERT INTO ruangan VALUES(NULL, '$x')");
-			header("location:index.php?alert=simpan");
+			mysqli_query($koneksi,"INSERT INTO gallery VALUES(NULL, '$ruangan', '$x')");
+			header("location:tambahruangan.php?alert=simpan");
 		}
 	}
-}
